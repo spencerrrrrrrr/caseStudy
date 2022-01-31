68b74753b948ba62c15d59f4006cc3e4f77a0ab5
Spencer Hurrle <spencer.hurrle@gmail.com>
2022-01-30 22:29:28 -0500

    Experimenting with Flask

--- spencer/app.py
+++ spencer/app.py
@@ -4,29 +4,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    html = '''
-    <title>Spencer Hurrle</title>
-    <h2>Professional Software Development and Process Automation</h2>
-    <h3>About Me:</h3>
-    <p>My name is Spencer Hurrle, and I’m currently a DevOps Student
-         at Per Scholas. I’m pursuing a career in DevOps so that I can 
-         fill out more of my personal potential. I always aim to achieve 
-         the most effective and transparent solutions in my work. In 
-         addition to being technically proficient, I love to collaborate 
-         with others and pool the best ideas into one amazing product.</p>
+    title = "Spencer Hurrle"
+#    html = '''
+#    <title>Spencer Hurrle</title>
+#    <h2>Professional Software Development and Process Automation</h2>
+#    <h3>About Me:</h3>
+#    <p>My name is Spencer Hurrle, and I’m currently a DevOps Student
+#         at Per Scholas. I’m pursuing a career in DevOps so that I can 
+#         fill out more of my personal potential. I always aim to achieve 
+#         the most effective and transparent solutions in my work. In 
+#         addition to being technically proficient, I love to collaborate 
+#         with others and pool the best ideas into one amazing product.</p>
 
-    <a href="{{ url_for('how-did-we-get-here') }}">Take a look at the process</a>
-    '''
-    #return render_template("index.html", title=title)
-    return html
+#    <a href="{{ url_for('how-did-we-get-here') }}">Take a look at the process</a>
+#    '''
+    return render_template("index.html", title=title)
+    #return html
 
 
 @app.route('/how-did-we-get-here')
 def how():
-    html = '''
-    <title>The Journey</title>
-    <h1>How Did We Get Here?</h1>
-    '''
-    return html
+    title = "The Journey"
+#    html = '''
+#    <title>The Journey</title>
+#    <h1>How Did We Get Here?</h1>
+#    '''
+#    return html
+    return render_template("journey.html", title=title)
 
 app.run(host='0.0.0.0', port=5000)
